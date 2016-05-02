527e47a38e5f526c777035e1c040994dde8597ef
qingyun meng <qingyun meng>
2016-05-02 23:29:27 +0800

    提交

---
+++ workspace3/LauncherActivity/src/com/example/launcheractivity/Prefs2Fragment.java
@@ -0,0 +1,17 @@
+package com.example.launcheractivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.widget.Toast;
+
+
+@SuppressLint("NewApi") public class Prefs2Fragment extends PreferenceFragment {
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.display_prefs);
+		String website=getArguments().getString("website");
+		Toast.makeText(getActivity(), "Õ¯’æ”Ú√˚ «"+website,Toast.LENGTH_LONG).show();;
+	}
+}
